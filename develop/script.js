9d4d6620fc38985aa86ff840b0c73b9028bf38cb
Janet Iqal <j.iqal35@gmail.com>
2021-06-18 21:36:21 -0500

    added pseudocode

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var generatePassword = " ";
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +13,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  
+
+// on click: series of prompts pop up for pass word criteria
+  // prompt.('Password criteria is..')
+
+// user selects which criteria to include
+
+//user decides how long the password is
+
+// user chooses character types in password
+  // (lowercase, uppercase, numeric, and/or special characters)
+
+// each prompt answered-- input is validated and at least one character type is selected
+
+//when all prompts answered, password is generated
+//password is either an alert or written on page 
